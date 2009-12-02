2a9f41a18dee783a63f366da6157c7307d1783c2
shimada <snoozer.05+zantare@gmail.com>
2009-12-02 15:16:34 +0900

    refactor Bowling::Game#score

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -13,13 +13,15 @@ module Bowling
 			roll_idx = 0
 			10.times do
 				if @rolls[roll_idx] == 10 # strike
-					score += @rolls[roll_idx] + @rolls[roll_idx+1] + @rolls[roll_idx + 2]
+					score += @rolls[roll_idx] #pins
+					score += @rolls[roll_idx+1] + @rolls[roll_idx + 2] #strike_bonus
 					roll_idx += 1
 				elsif  (@rolls[roll_idx] + @rolls[roll_idx+1]) == 10 #spare
-					score += @rolls[roll_idx] + @rolls[roll_idx+1] + @rolls[roll_idx + 2]
+					score += @rolls[roll_idx] + @rolls[roll_idx+1] #pins
+					score += @rolls[roll_idx + 2] #spare_bonus
 				  roll_idx += 2
 				else # others
-					score += @rolls[roll_idx] + @rolls[roll_idx+1]
+					score += @rolls[roll_idx] + @rolls[roll_idx+1] #pins
           roll_idx += 2 
 				end
 			end
