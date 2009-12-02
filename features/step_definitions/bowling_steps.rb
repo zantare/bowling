b42c6eb62b31ae0d79656f17fe74291d4b1db412
shimada <snoozer.05+zantare@gmail.com>
2009-12-02 11:35:27 +0900

    passed gutter scenario.

---
+++ features/step_definitions/bowling_steps.rb
@@ -0,0 +1,11 @@
+前提 /^ゲームを開始する$/ do
+	@game = Bowling::Game.new
+end
+
+もし /^すべての投球がガターである$/ do
+	20.times { @game.roll(0) }
+end
+
+ならば /^スコアは(\d+)点であること$/ do |score|
+	@game.score.should equal(score.to_i)
+end
---
+++ lib/bowling/game.rb
@@ -0,0 +1,10 @@
+module Bowling
+	class Game
+		def roll(pins)
+		end
+
+		def score
+			0
+		end
+	end
+end
