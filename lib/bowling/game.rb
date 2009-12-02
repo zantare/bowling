0b6edbad7ffee208f63a573f4f429f679d56e390
shimada <snoozer.05+zantare@gmail.com>
2009-12-02 16:22:09 +0900

    refactor Bowling::Game#score.
    extract Game#strike_bonus and Game#spare_bonus from Game#score.

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -14,11 +14,11 @@ module Bowling
 			10.times do
 				if roll_at(roll_idx) == 10 #strike
 			    score += roll_at(roll_idx) # pins
-					score += two_rolls_from(roll_idx+1) #strike_bonus
+					score += strike_bonus(roll_idx)
 					roll_idx += 1
 				elsif two_rolls_from(roll_idx) == 10 #spare
 					score += two_rolls_from(roll_idx) #pins
-					score += roll_at(roll_idx+2) #spare_bonus
+					score += spare_bonus(roll_idx)
 					roll_idx += 2
 				else
 					score += two_rolls_from(roll_idx) #pins
@@ -29,6 +29,14 @@ module Bowling
 		end
 
 		private
+		def strike_bonus(idx)
+			two_rolls_from(idx+1)
+		end
+
+		def spare_bonus(idx)
+			roll_at(idx+2)
+		end
+
 		def roll_at(idx)
 			@rolls[idx]
 		end
